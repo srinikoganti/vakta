01b0acc5ec42d1c8b1f3c471ee899c0bc9d89788
Srini Koganti <skoganti@gmail.com>
2012-08-09 11:14:26 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vakta::Application.config.secret_token = 'b4d9a58a73dc79394ec2c349688ce19787c474762c6f142aa0287c75f2aee980d44fc7291108d34870122403148f0e53ff5dfc7f3d39d256fc6c2008f4349648'
